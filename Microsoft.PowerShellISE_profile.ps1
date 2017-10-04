2ac355274d4cdaef51b77eebe8d070818ed5b379
Michael Dopp <MDopp@sorenson.com>
2017-10-04 14:21:39 -0600

    V1.1_PowerShellISEProfile
    V1.1_PowerShellISEProfile

---
+++ Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,53 @@
+﻿Set-Location C:\Windows\System32\
+$Shell.WindowTile="System32"
+
+Set-executionpolicy Unrestricted
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value '*'
+#Get-ExecutionPolicy Unrestricted
+Enable-PSRemoting -force
+<#
+Set-executionpolicy -scope currentuser -executionpolicy remotesigned
+Get-ExecutionPolicy RemoteSigned
+Get-Module -ListAvailable
+#>
+
+#Session log
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript ("C:\Powershell\PSLOG\" + (Get-Date -Format dd-MM-yyyy_HH-mm) + ".log")
+
+
+## Extend Maximum History Count
+$MaximumHistoryCount = 128
+
+Update-Help -force
+
+
+#Importing Active Directory Module
+Import-Module ActiveDirectory -ErrorAction SilentlyContinue
+Import-Module FailoverClusters -ErrorAction SilentlyContinue
+Import-Module BitLocker -ErrorAction SilentlyContinue
+Import-Module ClusterAwareUpdating -ErrorAction SilentlyContinue
+Import-Module WebAdministration -ErrorAction SilentlyContinue
+Import-Module RemoteDesktop -ErrorAction SilentlyContinue
+Import-Module VMware.VimAutomation.Core -ErrorAction SilentlyContinue
+Import-Module TabExpansionPlusPlus -ErrorAction SilentlyContinue
+Import-Module ZLocation -ErrorAction SilentlyContinue
+Import-Module AWSPowerShell -ErrorAction SilentlyContinue
+Import-Module SqlServer -ErrorAction SilentlyContinue
+Get-Module -ListAvailable
+#update-module
+#update-script
+
+
+
+
+Get-VICredentialStoreItem
+Connect-VIServer "slc-vcenter1.corp.srelay.com" -User "corp\mdopp" -Password "" -SaveCredentials
+Connect-VIServer "mesa-vcenter1.corp.srelay.com" -User "corp\mdopp" -Password "" -SaveCredentials
+Connect-VIServer "qa-vcenter2.corp.srelay.com" -User "corp\mdopp" -Password "" -SaveCredentials
+Connect-VIServer "edc-vcenter55.corp.srelay.com" -User "corp\mdopp" -Password "" -SaveCredentials
+Connect-VIServer "labvc1.corp.srelay.com" -User "corp\mdopp" -Password "" -SaveCredentials
+
+Cls
