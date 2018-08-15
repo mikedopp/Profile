bc71fa4ea77beb44a2118edde53faaaba9bff88d
Mike Dopp <mikedopp@mediconnect.net>
2018-08-15 16:38:00 -0600

    happy testingwithapps

---
+++ Microsoft.PowershellISE_profile.Tests.ps1
@@ -0,0 +1,9 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+ 
+Describe "Microsoft.PowershellISE_profile" {
+    It "outputs 'C:\Windows\System32\'" {
+        Get-HelloWorld | Should Be 'C:\Windows\System32\'
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+This is a test of using appveyor and git and adding a pester test to learn why this is sooo cool.
+![](file://C:/Users/mikedopp/Pictures/Think_Correctly__FreeBSD_by_xaimus.png)
